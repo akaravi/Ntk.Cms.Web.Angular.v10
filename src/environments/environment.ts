dd5181d828def08373ad11081407ba65091b549e
pipeline <pipeline@ntk.ir>
2025-07-22 08:46:46 +0330

    indo

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,7 +7,7 @@ import { EnvironmentModel } from '../app/core/models/environmentModel';
 import { LanguageFlagModel } from 'src/app/core/models/languageFlagModel';
 declare var require: any;
 
-export const environment:EnvironmentModel = {
+export const environment: EnvironmentModel = {
   production: false,
   checkAccess: false,
   consoleLog: false,
@@ -20,8 +20,8 @@ export const environment:EnvironmentModel = {
   mainTitle: 'سامانه مدیریتی محتوا',
   cmsServerConfig: {
     configApiRetry: 1,
-    //configApiServerPath: 'https://apicms.ir/api/v2/',
-    configApiServerPath: 'https://localhost:2390/api/v3/', // Test Api
+    configApiServerPath: 'https://apicms.ir/api/v3/',
+    //configApiServerPath: 'https://localhost:2390/api/v3/', // Test Api
     //configApiServerPath: 'https://6f4pj689-7130.euw.devtunnels.ms/api/v2/', // Test Api
     //configApiServerPath: 'https://localhost:44342/api/v2/', // Test Api Docer
     configHubServerPath: 'https://apicms.ir/hub/',
