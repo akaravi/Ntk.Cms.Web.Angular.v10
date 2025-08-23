620352896eb1b03a87ef126d3a17542d67701799
karavi <karavi.alireza@gmail.com>
2025-08-23 21:00:08 +0330

    indo fixed input

--- src/app/shared/progress-spinner/progress-spinner.component.ts
+++ src/app/shared/progress-spinner/progress-spinner.component.ts
@@ -3,18 +3,18 @@ import {
   Component,
   Input,
   OnDestroy,
-  OnInit
-} from '@angular/core';
-import { ProcessInfoModel } from 'ntk-cms-api';
-import { Subscription } from 'rxjs';
-import { ProcessService } from 'src/app/core/services/process.service';
-import { environment } from 'src/environments/environment';
+  OnInit,
+} from "@angular/core";
+import { ProcessInfoModel } from "ntk-cms-api";
+import { Subscription } from "rxjs";
+import { ProcessService } from "src/app/core/services/process.service";
+import { environment } from "src/environments/environment";
 
 @Component({
-  selector: 'app-progress-spinner',
-  templateUrl: './progress-spinner.component.html',
-  styleUrls: ['./progress-spinner.component.scss'],
-  standalone: false
+  selector: "app-progress-spinner",
+  templateUrl: "./progress-spinner.component.html",
+  styleUrls: ["./progress-spinner.component.scss"],
+  standalone: false,
 })
 export class ProgressSpinnerComponent implements OnInit, OnDestroy {
   static nextId = 0;
@@ -24,25 +24,34 @@ export class ProgressSpinnerComponent implements OnInit, OnDestroy {
     private cdr: ChangeDetectorRef,
   ) {
     this.processService.cdr = this.cdr;
-    this.unsubscribe.push(this.processService.processSubject.subscribe((value) => {
-      if (value) {
-        this.infoArea = value.infoArea[this.optionsInfoAreaId];
-        this.inRunArea = value.inRunArea[this.optionsInfoAreaId];
-      } else {
-        this.infoArea = [];
-        this.inRunArea = [];
-      }
-      if (environment.ProgressConsoleLog)
-        console.log("ProgressSpinnerComponent infoArea", this.infoArea);
-    }));
+    this.unsubscribe.push(
+      this.processService.processSubject.subscribe((value) => {
+        if (value?.infoArea && value.infoArea[this.infoAreaId]) {
+          this.infoArea = value.infoArea[this.infoAreaId];
+        } else {
+          this.infoArea = [];
+        }
+        if (value?.inRunArea && value.inRunArea[this.infoAreaId]) {
+          this.inRunArea = value.inRunArea[this.infoAreaId];
+        } else {
+          this.inRunArea = [];
+        }
+        if (environment.ProgressConsoleLog)
+          console.log("ProgressSpinnerComponent infoArea", this.infoArea);
+      }),
+    );
   }
-  @Input() optionsInfoAreaId: string = 'global';
+
+  @Input() set optionsInfoAreaId(str: string) {
+    if (str?.length > 0) this.infoAreaId = str;
+    else this.infoAreaId = "global";
+    this.cdr.detectChanges();
+  }
+  infoAreaId = "global";
   inRunArea: boolean[] = [];
   infoArea: Map<string, ProcessInfoModel>[] = [];
   private unsubscribe: Subscription[] = [];
-  ngOnInit(): void {
-
-  }
+  ngOnInit(): void {}
   ngOnDestroy() {
     this.unsubscribe.forEach((sb) => sb.unsubscribe());
   }
