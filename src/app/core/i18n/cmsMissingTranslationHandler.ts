73a3f2cc194e3ca5210e7bcdf43c4c4e650947e1
karavi <karavi.alireza@gmail.com>
2025-09-01 09:48:55 +0330

    indo

--- src/app/core/i18n/cmsMissingTranslationHandler.ts
+++ src/app/core/i18n/cmsMissingTranslationHandler.ts
@@ -7,7 +7,12 @@ import {
 @Injectable()
 export class CmsMissingTranslationHandler implements MissingTranslationHandler {
   handle(params: MissingTranslationHandlerParams) {
-    console.log(`|${params.translateService.currentLang}:?${params.key}?|`);
+    if (
+      params.key.indexOf("filemanager.") < 0 &&
+      params.key.indexOf("CRON.") < 0 &&
+      params.key.indexOf("querybuilder.") < 0
+    )
+      console.log(`|${params.translateService.currentLang}:?${params.key}?|`);
     return `|?${params.key}?|`;
   }
 }
